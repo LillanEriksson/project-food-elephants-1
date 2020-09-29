546cb98dff12bbc9f4fb653f8f2483c514a34302
LillanEriksson <nailsbylillan@hotmail.com>
2020-09-29 16:49:55 +0100

    started with feature

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
+const API_KEY = '7d6e8cfb6aa88bd5bc4dc97bf6908874';
+const API_URL =
+  'https://developers.zomato.com/api/v2.1/search?entity_id=61&entity_type=city&cuisines=55';
 
-const API_KEY = "7d6e8cfb6aa88bd5bc4dc97bf6908874";
-const API_URL = "https://developers.zomato.com/api/v2.1/search?entity_id=61&entity_type=city&cuisines=55";
+const mainContainer = document.getElementById('mainContainer');
 
-const mainContainer = document.getElementById("mainContainer");
-
-fetch(API_URL, {headers: {"user-key": API_KEY}})
+fetch(API_URL, { headers: { 'user-key': API_KEY } })
   .then((response) => {
     return response.json();
   })
   .then((italian) => {
-    //console.log(italian.restaurants);
+    console.log(italian.restaurants);
 
     const restaurantArray = italian.restaurants;
-    restaurantArray.forEach(restaurant => {
-      const selectedRestaurant = restaurant.restaurant; 
+    restaurantArray.forEach((restaurant) => {
+      const selectedRestaurant = restaurant.restaurant;
+
       mainContainer.innerHTML += `<div class="restaurant-container">
       <img src="${selectedRestaurant.featured_image}" />
       <h2>${selectedRestaurant.name} ${selectedRestaurant.location.locality}</h2>
       <p>Average cost: ${selectedRestaurant.average_cost_for_two}</p>
       <p>Rating: ${selectedRestaurant.user_rating.aggregate_rating}</p>
-      </div>`; 
-      
-
-
+      </div>`;
     });
-  })
+    restaurantArray.map() => {
+      const restaurantId = resId.restaurant.R.res_id;
+      return restaurantId;
 
-  
+      //let forEachBook = []; data.items.forEach(book => { const author = book.volumeInfo.authors; const title = book.////volumeInfo.title; const description = book.volumeInfo.description; const singleObject = { author, title, ////
+      //description }; forEachBook.push(singleObject); });
+    });
+  });
