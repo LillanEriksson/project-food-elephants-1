eaf193dedde42f9dc54f001860535bb6fef3eb3c
Corneliaryas <hello@corneliaryas.com>
2020-09-29 14:56:02 +0200

    Step 2 done

--- script.js
+++ script.js
@@ -5,26 +5,25 @@ const API_URL = "https://developers.zomato.com/api/v2.1/search?entity_id=61&enti
 const mainContainer = document.getElementById("mainContainer");
 
 fetch(API_URL, {headers: {"user-key": API_KEY}})
-  // .then(res => res.json())
   .then((response) => {
     return response.json();
   })
   .then((italian) => {
-    console.log(italian.restaurants);
+    //console.log(italian.restaurants);
 
     const restaurantArray = italian.restaurants;
     restaurantArray.forEach(restaurant => {
-      const selectedRestaurant = restaurant.restaurant;
-      console.log(selectedRestaurant.name);
-      console.log(`Average cost: ${selectedRestaurant.average_cost_for_two}`);
-      console.log(`Address: ${selectedRestaurant.location.address}`);
-      
-      mainContainer.innerHTML += `<img src="${selectedRestaurant.featured_image}" />`; 
+      const selectedRestaurant = restaurant.restaurant; 
+      mainContainer.innerHTML += `<div class="restaurant-container">
+      <img src="${selectedRestaurant.featured_image}" />
+      <h2>${selectedRestaurant.name} ${selectedRestaurant.location.locality}</h2>
+      <p>Average cost: ${selectedRestaurant.average_cost_for_two}</p>
+      <p>Rating: ${selectedRestaurant.user_rating.aggregate_rating}</p>
+      </div>`; 
       
 
 
     });
-    // console.log(italian.restaurants.restaurant.name);
   })
 
   
