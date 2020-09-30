2595590b9c12b4a88211fe4c19e0876fc0d142a0
Rebkova <rebkova@gmail.com>
2020-09-30 11:27:11 +0200

    display 3 reviews for one restaurant

--- script.js
+++ script.js
@@ -4,6 +4,37 @@ const API_URL =
 
 const mainContainer = document.getElementById('mainContainer');
 
+
+//fetch restaurant reviews
+const REVIEWS_URL = "https://developers.zomato.com/api/v2.1/reviews?res_id=6113850";
+
+fetch(REVIEWS_URL, { headers: { 'user-key': API_KEY } })
+  .then((response) => {
+    return response.json();
+  })
+  .then((reviews) => {
+    //start function 
+    const reviewsArray = reviews.user_reviews;
+
+    //1) sort based on timestamp try slice() first
+    const slicedArray = reviewsArray.slice(0, 3);
+    console.log(`Sliced array: ${slicedArray}`);
+
+    //2) choose the first 3 -> use slice() based on index -> save in new variable (array) - forEach on that array
+
+    //loop over array of reviews and print them out
+    slicedArray.forEach((review) => {
+      var userReview = review.review.rating_text;
+      var userReviewTime = review.review.review_time_friendly;
+      // console.log(`Review: ${userReview}`);
+      // console.log(`Review time: ${userReviewTime}`);
+      mainContainer.innerHTML += `<p>${userReview}, ${userReviewTime}</p>`;
+    })
+
+  })
+
+
+
 //fetch restaurant info
 fetch(API_URL, { headers: { 'user-key': API_KEY } })
   .then((response) => {
@@ -22,6 +53,7 @@ fetch(API_URL, { headers: { 'user-key': API_KEY } })
       <p>Average cost: ${selectedRestaurant.average_cost_for_two}</p>
       <p>Rating: ${selectedRestaurant.user_rating.aggregate_rating}</p>
       </div>`;
+      //incorporate function with <p> and review?
     });
 
     //loop through restaurants and print out res_ids
@@ -34,22 +66,11 @@ fetch(API_URL, { headers: { 'user-key': API_KEY } })
       console.log(`Restaurant ID: ${restaurantId}`);
       return restaurantId;
     });
-   
 
-      //let forEachBook = []; data.items.forEach(book => { const author = book.volumeInfo.authors; const title = book.////volumeInfo.title; const description = book.volumeInfo.description; const singleObject = { author, title, ////
-      //description }; forEachBook.push(singleObject); });
+
+    //let forEachBook = []; data.items.forEach(book => { const author = book.volumeInfo.authors; const title = book.////volumeInfo.title; const description = book.volumeInfo.description; const singleObject = { author, title, ////
+    //description }; forEachBook.push(singleObject); });
   });
 
-  //fetch restaurant reviews
-  const REVIEWS_URL = "https://developers.zomato.com/api/v2.1/reviews?res_id=6113850";
 
-  fetch(REVIEWS_URL, {headers: {'user-key': API_KEY} })
-    .then((response) => {
-      return response.json();
-    })
-    .then((reviews) => {
-      console.log(`Reviews: ${reviews.}`);
-      mainContainer.innerHTML += `<p>${reviews}</p>`;
-
-    })
 
