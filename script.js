e82de17a3a30ed764b7fbad6b5aab245343d71ae
Rebkova <rebkova@gmail.com>
2020-09-30 09:01:48 +0200

    added new fetch for reviews

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const API_URL =
 
 const mainContainer = document.getElementById('mainContainer');
 
+//fetch restaurant info
 fetch(API_URL, { headers: { 'user-key': API_KEY } })
   .then((response) => {
     return response.json();
@@ -22,11 +23,33 @@ fetch(API_URL, { headers: { 'user-key': API_KEY } })
       <p>Rating: ${selectedRestaurant.user_rating.aggregate_rating}</p>
       </div>`;
     });
-    restaurantArray.map() => {
+
+    //loop through restaurants and print out res_ids
+    // restaurantArray.forEach((resId) => {
+    //   const restaurantId = resId.restaurant.R.res_id;
+    //   console.log(`Restaurant ID ${restaurantId}`);
+
+    restaurantArray.map((resId) => {
       const restaurantId = resId.restaurant.R.res_id;
+      console.log(`Restaurant ID: ${restaurantId}`);
       return restaurantId;
+    });
+   
 
       //let forEachBook = []; data.items.forEach(book => { const author = book.volumeInfo.authors; const title = book.////volumeInfo.title; const description = book.volumeInfo.description; const singleObject = { author, title, ////
       //description }; forEachBook.push(singleObject); });
-    });
   });
+
+  //fetch restaurant reviews
+  const REVIEWS_URL = "https://developers.zomato.com/api/v2.1/reviews?res_id=6113850";
+
+  fetch(REVIEWS_URL, {headers: {'user-key': API_KEY} })
+    .then((response) => {
+      return response.json();
+    })
+    .then((reviews) => {
+      console.log(`Reviews: ${reviews.}`);
+      mainContainer.innerHTML += `<p>${reviews}</p>`;
+
+    })
+
